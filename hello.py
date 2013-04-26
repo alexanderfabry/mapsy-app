ad2e8077e751fa2cd7324b2451c9d8d558a6216e
Claudiu-Vlad Ursache <claudiu.vlad.ursache@gmail.com>
2013-04-26 04:02:44 +0200

    Another try to restore the template.

--- hello.py
+++ hello.py
@@ -1,27 +1,13 @@
 import os
 from flask import Flask
-import jinja2
-
-
-
-app = Flask(__name__)
+from jinja2 import Environment, PackageLoader
 
 env = Environment(loader=PackageLoader('app', 'templates'))
 template = env.get_template('test_template.html')
 
-# def include_file(name):
-#     return jinja2.Markup(loader.get_source(env, name)[0])
+app = Flask(__name__)
 
-# loader = jinja2.PackageLoader(__name__, 'templates')
-# env = jinja2.Environment(loader=loader)
-# env.globals['include_file'] = include_file
 
 @app.route('/')
 def hello():
     return template.render(the='variables', go='here')
-
-
-
-
-
-
