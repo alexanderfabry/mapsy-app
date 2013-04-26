dee36c0655ca3942ca10df120d0bc5d1b042358e
Claudiu-Vlad Ursache <claudiu.vlad.ursache@gmail.com>
2013-04-26 03:21:01 +0200

    Add test template.

--- hello.py
+++ hello.py
@@ -3,7 +3,11 @@ from flask import Flask
 from jinja2 import Environment, PackageLoader
 
 env = Environment(loader=PackageLoader('app', 'templates'))
+template = env.get_template('test_template.html')
+
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def hello():
