64982fc983ed6f9806a599b67d37fbe448dae714
Claudiu-Vlad Ursache <claudiu.vlad.ursache@gmail.com>
2013-04-26 04:00:58 +0200

    Try to fix the template.

--- hello.py
+++ hello.py
@@ -2,17 +2,19 @@ import os
 from flask import Flask
 import jinja2
 
-env = Environment(loader=PackageLoader('app', 'templates'))
-template = env.get_template('test_template.html')
+
 
 app = Flask(__name__)
 
-def include_file(name):
-    return jinja2.Markup(loader.get_source(env, name)[0])
+env = Environment(loader=PackageLoader('app', 'templates'))
+template = env.get_template('test_template.html')
 
-loader = jinja2.PackageLoader(__name__, 'templates')
-env = jinja2.Environment(loader=loader)
-env.globals['include_file'] = include_file
+# def include_file(name):
+#     return jinja2.Markup(loader.get_source(env, name)[0])
+
+# loader = jinja2.PackageLoader(__name__, 'templates')
+# env = jinja2.Environment(loader=loader)
+# env.globals['include_file'] = include_file
 
 @app.route('/')
 def hello():
