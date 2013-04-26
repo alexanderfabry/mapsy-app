9a0b06329ac90e2fd20fe09117cb8227dc7ac156
Claudiu-Vlad Ursache <claudiu.vlad.ursache@gmail.com>
2013-04-26 03:29:43 +0200

    Update template to deliver static files (if it works).

--- hello.py
+++ hello.py
@@ -8,6 +8,11 @@ template = env.get_template('test_template.html')
 app = Flask(__name__)
 
 
+def get_resource_as_string(name, charset='utf-8'):
+    with app.open_resource(name) as f:
+        return f.read().decode(charset)
+
+app.jinja_env.globals['get_resource_as_string'] = get_resource_as_string
 
 @app.route('/')
 def hello():
