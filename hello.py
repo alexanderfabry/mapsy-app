a9c43dcab78a003e5157e3c444a6bb9630e3595b
Claudiu-Vlad Ursache <claudiu.vlad.ursache@gmail.com>
2013-04-26 03:18:39 +0200

    Experiment with jinja2.

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
+from jinja2 import Environment, PackageLoader
 
+env = Environment(loader=PackageLoader('app', 'templates'))
 app = Flask(__name__)
 
 @app.route('/')
